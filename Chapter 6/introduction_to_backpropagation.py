fc11303a9de90d147ef52fa49d12fea1d7f7f5a1
palacionis <jonas.palacionis@gmail.comm>
2020-11-02 22:29:39 +0200

    Adding 6th Chapter - BackPropagation

--- Chapter 6/introduction_to_backpropagation.py
+++ Chapter 6/introduction_to_backpropagation.py
@@ -68,11 +68,9 @@ import numpy as np
 
 np.random.seed(1)
 
-streetlights = np.array(
-    [[1, 0, 1], [0, 1, 1], [0, 0, 1], [1, 1, 1], [0, 1, 1], [1, 0, 1]]
-)
+streetlights = np.array([[1, 0, 1], [0, 1, 1], [0, 0, 1], [1, 1, 1]])
 
-walk_vs_stop = np.array([0, 1, 0, 1, 1, 0])
+walk_vs_stop = np.array([[1, 1, 0, 0]]).T
 
 
 def relu(x):
@@ -87,9 +85,10 @@ alpha = 0.2
 hidden_layer_size = 4
 
 # random weights from the first layer to the second
-weights_0_1 = 2 * np.random.random((3, hidden_layer_size)) - 1
+weights_0_1 = 4 * np.random.random((3, hidden_layer_size)) - 3
 # random weights from the second layer to the output
-weights_1_2 = 2 * np.random.random((hidden_layer_size, 1)) - 1
+weights_1_2 = 4 * np.random.random((hidden_layer_size, 1)) - 3
+
 
 for iteration in range(60):
     layer_2_error = 0
@@ -98,22 +97,21 @@ for iteration in range(60):
         layer_1 = relu(np.dot(layer_0, weights_0_1))
         layer_2 = relu(np.dot(layer_1, weights_1_2))
 
-        layer_2_error += np.sum(layer_2 - walk_vs_stop[i : i + 1]) ** 2
+        layer_2_error += np.sum((layer_2 - walk_vs_stop[i : i + 1])) ** 2
 
-        layer_2_delta = walk_vs_stop[i : i + 1] - layer_2
+        layer_2_delta = layer_2 - walk_vs_stop[i : i + 1]
         layer_1_delta = layer_2_delta.dot(weights_1_2.T) * relu2deriv(layer_1)
 
-        weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
-        weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
+        weights_1_2 -= alpha * layer_1.T.dot(layer_2_delta)
+        weights_0_1 -= alpha * layer_0.T.dot(layer_1_delta)
 
-    if iteration % 9 == 0:
+    if iteration % 10 == 9:
         print(f"Error: {layer_2_error}")
 
 
-# Error: 3.1120884967431595
-# Error: 0.31298811341557736
-# Error: 0.0540487565336386
-# Error: 0.005919784786873184
-# Error: 0.00047166834729714375
-# Error: 3.338825575496048e-05
-# Error: 2.2837145520392978e-06
+# Error: 0.6342311598444467
+# Error: 0.35838407676317513
+# Error: 0.0830183113303298
+# Error: 0.006467054957103705
+# Error: 0.0003292669000750734
+# Error: 1.5055622665134859e-05
